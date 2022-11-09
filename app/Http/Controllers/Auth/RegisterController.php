9d1df733f70384f5185a34aa0339795bc46ce128
yuta-yatsuka <yuta.yatsuka@ponion.jp>
2022-11-09 12:22:19 +0900

    登録の際にユーザー情報にアクセストークンを設定するよう修正

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use App\Service\ShopifyStorefrontApi;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -64,10 +65,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $shopify = new ShopifyStorefrontApi();
+        $shopify->createCustomer($data['email'], $data['password']);
+
+        $accessToken = $shopify->getCustomerAccessToken($data['email'], $data['password']);
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'access_token' => $accessToken,
         ]);
     }
 }
